347729682753002019e9698406f32697a9020cae
AditiSingh2003 <adithakurti@gmail.com>
2023-01-18 02:43:35 +0530

    Added Item Details page

--- client/src/App.js
+++ client/src/App.js
@@ -29,7 +29,7 @@ function App() {
         <ScrollToTop />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path= "item/itemId" element={<ItemDetails />} />
+          <Route path= "item/:itemId" element={<ItemDetails />} />
           <Route path="checkout/success" element={<Confirmation />} />
           </Routes>
           <CartMenu/>
